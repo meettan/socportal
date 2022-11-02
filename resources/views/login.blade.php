1f3b3b819ab3500d9fc0cf520145811045c64225
SSSWeb-1 <opentech4u@gmail.com>
2022-11-02 14:39:48 +0530

    chnage

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,11 +14,8 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet">
-    <!--font-family: 'Roboto', sans-serif;-->
-    <link href="https://fonts.googleapis.com/css2?family=Gorditas:wght@400;700&display=swap" rel="stylesheet">
-    <!--font-family: 'Gorditas', cursive;-->
-</head>
-
+        <link href="https://fonts.googleapis.com/css2?family=Gorditas:wght@400;700&display=swap" rel="stylesheet">
+    </head>
 <body>
     <div class="LoginCardLayout">
         <div class="LoginCardLayout-card">
