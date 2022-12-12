14f3a35507aac54a6c1a6fb506c370df0cc06652
meettan <meettan@gmail.com>
2022-12-12 11:15:04 +0530

    chnage

--- resources/views/outer_page/privacypolicy.blade.php
+++ resources/views/outer_page/privacypolicy.blade.php
@@ -3,7 +3,7 @@
 <div class="wrapperCus">
 <div class="innerPageSecCus">
 <h2 >Privacy Policy: </h2>
-<p ><strong >BENFED</strong> or <a href="https://customer.benfed.in" ><strong ><u>https://customer.benfed.in</u></strong></a> (mentioned as "we" or "our" here) is committed to protecting and respecting your privacy. </p>
+<!-- <p ><strong >BENFED</strong> or <a href="https://customer.benfed.in" ><strong ><u>https://customer.benfed.in</u></strong></a> (mentioned as "we" or "our" here) is committed to protecting and respecting your privacy. </p>
 <p >  This policy (together with our terms of use and any other documents referred to on it) sets out the basis on which any personal data we collect from you, or that you provide to us, will be processed by us. Please read the following carefully to understand our views and practices regarding your personal data and how we will treat it. By https://customer.benfed.in you are accepting and consenting to the practices described in this policy. </p>
 <h3 >INFORMATION WE MAY COLLECT FROM YOU </h3>
 <p >  We may collect and process the following data about you: </p>
@@ -59,7 +59,157 @@
 <h3>CHANGES TO OUR PRIVACY POLICY </h3>
 <p>  Any changes we may make to our privacy policy in the future will be posted on this page and, where appropriate, notified to you by e-mail. Please check back frequently to see any updates or changes to our privacy policy. </p>
 <h3>CONTACT </h3>
-<p>  Questions, comments and requests regarding this privacy policy are welcomed and you should contact us. </p>
+<p>  Questions, comments and requests regarding this privacy policy are welcomed and you should contact us. </p> -->
+<p><strong >The BENFED (The West Bengal State Co-operative Marketing Federation Ltd.)</strong >
+(hereinafter referred to as &quot;we&quot; or &quot;our&quot;) through its portal <a href="https://customer.benfed.in" ><strong ><u>https://customer.benfed.in</u></strong></a>
+is committed to protecting and respecting privacy of the client.</p>
+<p>This policy (together with our terms of use and any other documents referred to on it) sets
+out the basis on which any institutional / business / transaction data we collect from the
+client, or that the client provide to us, would be processed by us. Please read the
+following carefully to understand our views and practices regarding institutional / business
+/ transaction data of the client and how we would treat it. By using the portal <a href="https://customer.benfed.in" ><strong ><u>https://customer.benfed.in</u></strong></a> the clients are accepting and consenting to the practices described in
+this policy.</p>
+<h3>INFORMATION WE MAY COLLECT FROM THE CLIENT:</h3>
+<p>We may collect and process the following data about the client:</p>
+<ol>
+<li> Information the clients give us. The client may give us information about the
+organisation / entity by filling in forms on our site <a href="https://customer.benfed.in" ><strong ><u>https://customer.benfed.in</u></strong></a> or by
+corresponding with us by phone, e-mail or otherwise. This includes information the client
+provide when the organisation / entity register to use our site, subscribe to our service,
+search for a product, participate in discussion boards or other social media functions on
+our site, enter a competition, promotion or survey and when the client report a problem
+with our site. The information the clients give us may include the name, address, e-mail
+address and phone number, PAN No., GSTIN, financial and credit card information,
+organisational description and photograph of the representative of the organisation /
+entity.</li>
+<li> Information we collect about the client. With each of the clients visit to our site we
+may automatically collect the following information:
+Technical Information: technical information, including the Internet protocol (IP) address
+used to connect the client’s computer to the Internet, the client’s login information,
+browser type and version, time zone setting, browser plug-in types and versions,
+operating system and platform;
+Information about the client: we collect information about the client while visiting the
+website, includes the full Uniform Resource Locators (URL) clickstream to, through and
+from our site (including date and time); services / products the client viewed or
+searched for; page response times, download errors, length of visits to certain pages,
+page interaction information (such as scrolling, clicks, and mouse-overs), and methods
+used to browse away from the page and any phone number used to call our customer
+service number.</li>
+<li> Information we receive from other sources. We may receive information about the
+client, if the client use any of the other websites we operate or the other services we
+provide. We are also working closely with third parties (including, for example, business
+partners, sub-contractors in technical, payment and delivery services, advertising
+networks, analytics providers, search information providers, credit reference agencies)
+
+and may receive institutional / business / transaction information about the client from
+them.</li>
+<li>Geo-Location Information. Some devices allow applications to access real-time location
+based information (for example GPS). Our mobile apps may collect such information from
+the client’s mobile device at any time while the client would download or use our mobile
+apps. We may use this information to optimise the client’s experience.</li>
+</ol>
+<h3> USES MADE OF THE INFORMATION: </h3>
+<p>We use institutional / business / transaction and related information held about the client
+in the following ways:</p>
+<p>to carry out our obligations arising from any contracts entered between the client
+and us and to provide the client with the information, products and services
+that the client request from us;</p>
+<p>to provide the client with information about other goods and services we offer that
+are like those that the client has already purchased or enquired about;</p>
+<p>to provide the client, or permit selected third parties to provide the client, with
+information about goods or services we feel may interest the client;</p>
+<p>to notify the client about changes to our service;</p>
+<p>to understand customer requirements, perform analysis and comparisons;</p>
+<p>to develop and test products and services;</p>
+<p>to improve our site and ensure that content from our site is presented in the most
+effective manner for the client and for the client’s computer;</p>
+<p>to administer our site and for internal operations, including troubleshooting, data
+analysis, testing, research, statistical and survey purposes;</p>
+<p>to allow the client to participate in interactive features of our service, when it
+chooses to do so; and</p>
+<p>to keep our site safe and secure;</p>
+<p>We may combine the information we receive from other sources with information the
+client gives to us and information we collect about the client. We may us this information
+and the combined information for the purposes set out above (depending on the types of
+information we receive).</p>
+<h3>DISCLOSURE OF THE INFORMATION ABOUT THE CLIENT:</h3>
+<p>We may share institutional / business / transaction &amp; related information of the client
+with selected third parties including: </p>
+<ol>
+<li> Business partners, suppliers and sub-contractors for the performance of any contract
+we enter with them or you, the client.</li>
+
+<li> Analytics and search engine providers that assist us in the improvement and
+optimisation of our site.</li>
+</ol>
+<p>We may disclose the client’s institutional / business / transaction &amp; related information to
+third parties: </p>
+<p>a) In the event selling or buying any business or assets by us, we may disclose the
+client’s institutional / business / transaction &amp; related data to the prospective
+seller or buyer of such business or assets.</p>
+<p>b) If BENFED or substantially all assets of BENFED are acquired by a third party, in
+which case institutional / business / transaction &amp; related data held by it about its
+clients will be one of the transferred assets.</p>
+<p>c) If we are under a duty to disclose or share the client’s institutional / business /
+transaction &amp; related data in order to comply with any legal obligation, or in order
+to enforce or apply our terms of use and other agreements; or to protect the
+rights, property, or safety of BENFED, our customers, or others.</p>
+<p>This includes exchanging information with other companies and organisations for the
+purposes of fraud protection. </p>
+<h3>WHERE WE STORE THE CLIENTR INSTITUTIONAL DATA:</h3>
+<p>The data that we collect from the client may be transferred to, and stored at, a
+destination inside India. It may also be processed by staff operating outside the India who
+works for us or for one of our suppliers. Such staff may be engaged in, among other things,
+the fulfilment of the client’s order, the processing of the client’s payment details and the
+provision of support services. By submitting the client’s institutional data, the client
+agrees to this transfer, storing or processing. We will take all steps reasonably necessary
+to ensure that the client’s data is treated securely and in accordance with this privacy
+policy.</p>
+<p>Where we have given the client (or where the client have chosen) a password which
+enables the client to access certain parts of our site, the client are responsible for keeping
+this password confidential. We ask the client not to share a password with anyone.</p>
+<p>Unfortunately, the transmission of information via the internet is not completely secure.
+Although we will do our best to protect the client’s institutional data, we cannot
+guarantee the security of the client’s data transmitted to our site; any transmission is at
+the client’s own risk. Once we have received the client’s information, we will use strict
+procedures and security features to try to prevent unauthorised access.</p>
+<h3>THE CLIENT’S RIGHTS: </h3>
+<p>The clients have the right to ask us not to process the client’s institutional data for
+marketing purposes. We will usually inform the client (before collecting the client’s data)
+if we intend to use the client’s data for such purposes or if we intend to disclose the
+client’s information to any third party for such purposes. The client can exercise its right
+to prevent such processing by checking certain boxes on the forms we use to collect the
+client’s data. The client can also exercise the right at any time by contacting us.</p>
+
+<p>Our site may, from time to time, contain links to and from the websites of our partner
+networks, advertisers and affiliates. If the client follow a link to any of these websites,
+please note that these websites have their own privacy policies and that we do not accept
+any responsibility or liability for these policies. Please check these policies before the
+client submit any institutional data to these websites.</p>
+<h3>ACCESS TO INFORMATION:</h3>
+<p>The Information &amp;Technology Act 2000, as amended time to time, gives the client the
+right to access information held about their own. The client’s right of access can be
+exercised in accordance with the Act. Any access request may be subject to a fee to meet
+our costs in providing the client with details of the information we hold about the client.</p>
+<h3>COOKIES:</h3>
+<p>A cookie is a small file, typically of letters and numbers, downloaded onto a device such
+as a computer when a user accesses certain websites. Cookies allow a website to recognise
+a user&#39;s device.</p>
+<p>We use cookies to collate general management data which is used to plan enhancement to
+the client’s service. We never store any institutional information in cookies (on any
+computer that the client may use) that can be used to identify the client, such as the
+client’s name.</p>
+<p>If the client does not want us to use cookies, the client may be able to configure the
+client’s browser not to accept them. Please refer to its browser&#39;s &quot;help&quot; facility for further
+information. However, the client should bear in mind that this may affect the client’s
+ability to use the services we provide.</p>
+<h3>CHANGES TO OUR PRIVACY POLICY:</h3>
+<p>Any changes we may make to our privacy policy in the future will be posted on this page
+and, where appropriate, notified to the client by e-mail. Please check back frequently to
+see any updates or changes to our privacy policy.</p>
+<h3>CONTACT:</h3>
+<p>Questions, comments and requests regarding this privacy policy are welcomed and the
+client should contact us.</p>
 </div>
 </div>
 
--- resources/views/outer_page/termcondition.blade.php
+++ resources/views/outer_page/termcondition.blade.php
@@ -4,7 +4,8 @@
 <div class="innerPageSecCus">
 
 <h2 >Terms & Condition  </h2>
-<p >  By using this website, you agree to our terms of use as set out below. </p>
+
+<!-- <p >  By using this website, you agree to our terms of use as set out below. </p>
 <h3 >Exclusion of liability </h3>
 <p >  These 'Exclusion of Liability' provisions should be read carefully as they exclude or limit our legal liability in connection with your use of this website. Nothing in these terms and conditions and in particular within the 'Exclusion of Liability' clauses shall attempt to exclude liability that is not permissible under applicable law, including without limitation, for death or personal injury, or for fraudulent misrepresentation.</p>
 <p >  While we have taken all reasonable steps to ensure the accuracy and completeness of the content of the website, we exclude any warranties, undertakings or representations (either express or implied) to the full extent permitted under applicable law, that the website or (including without limitation) all or any part of the content or materials, accuracy, availability or completeness of the content of the website or any part of the content or materials are appropriate or available for use in any jurisdiction where we provide our services. If you use this website from other jurisdictions, you are responsible for compliance with applicable local laws.</p>
@@ -27,7 +28,100 @@
 <h3 >Security</h3>
 <p >  You must keep secret any personal identification numbers or other security codes that we may give you for access to <strong >BENFED &lsquo;s</strong> service, and comply with all reasonable instructions that <strong >BENFED </strong>may issue in relation to keeping such personal details safe. You should also make sure that any Internet payment transaction you make is done in a secure environment.</p>
 <h3 >Rights</h3>
-<p ><strong >BENFED </strong>holds the intellectual property rights in the contents of this website and all rights are reserved. You may download, store in cache, display and print the material and information in an unaltered form for your own personal or internal business purposes only provided that this notice appears with all copies. You must not systematically retrieve data or other content from this website to create or compile, directly or indirectly, a collection, compilation, database or directory, or, copy, re-transmit, distribute or commercialise any material or information on this website, without <strong >BENFED &lsquo;s</strong> prior written permission. </p>
+<p ><strong >BENFED </strong>holds the intellectual property rights in the contents of this website and all rights are reserved. You may download, store in cache, display and print the material and information in an unaltered form for your own personal or internal business purposes only provided that this notice appears with all copies. You must not systematically retrieve data or other content from this website to create or compile, directly or indirectly, a collection, compilation, database or directory, or, copy, re-transmit, distribute or commercialise any material or information on this website, without <strong >BENFED &lsquo;s</strong> prior written permission. </p> -->
+
+
+
+<p>The BENFED (The West Bengal State Co-operative Marketing Federation Ltd.)
+(hereinafter referred to as &quot;we&quot; or &quot;our&quot;) through its portal <a href="https://customer.benfed.in" ><strong ><u>https://customer.benfed.in</u></strong></a> 
+presumes that the clients agree to our terms of use as set out below.<p>
+<h3 >Exclusion of liability </h3>
+<p > These &#39;Exclusion of Liability&#39; provisions should be read carefully as they exclude or limit
+our legal liability in connection with the client’s use of this website
+<a href="https://customer.benfed.in" ><strong ><u>https://customer.benfed.in</u></strong></a>  Nothing in these terms and conditions and in particular
+within the &#39;Exclusion of Liability&#39; clauses shall attempt to exclude liability that is not
+permissible under applicable law, including without limitation, for death or personal
+injury, or for fraudulent misrepresentation.</p>
+<p > While we have taken all reasonable steps to ensure the accuracy and completeness of the
+content of the website, we exclude any warranties, undertakings or representations
+(either express or implied) to the full extent permitted under applicable law, that the
+website or (including without limitation) all or any part of the content or materials,
+accuracy, availability or completeness of the content of the website or any part of the
+content or materials are appropriate or available for use in any jurisdiction where we
+provide our services. If the client use this website <a href="https://customer.benfed.in" ><strong ><u>https://customer.benfed.in</u></strong></a>  from other
+jurisdictions, the clients are responsible for compliance with applicable local laws.</p>
+<p > We accept no liability (including without limitation) in either contract, tort, negligence,
+statutory duty or otherwise (to the maximum extent permitted by applicable law) arising
+out of the use of or access to this website <a href="https://customer.benfed.in" ><strong ><u>https://customer.benfed.in</u></strong></a>  (which includes
+without limitation) any errors or omissions contained in this website or if the website is
+unavailable and we shall not be liable for any direct or indirect:</p>
+  <p ><strong >Economic losses</strong> (including without limitation loss of revenues, data, profits, contracts,
+use, opportunity, business or anticipated savings);</p>
+<ol>
+<li>Loss of goodwill or reputation;</li>
+<li>Special, incidental, consequential loss or damage,</li>
+<li>Suffered or incurred arising out of or in connection with your use of this website
+and these terms and conditions.</li>
+</ol>
+<p>Access to and use of this website is at the user&#39;s own risk and we do not warrant that the
+use of this website or any material downloaded from it will not cause damage to any
+property, or otherwise minimise or eliminate the inherent risks of the internet including
+but not limited to loss of data, computer virus infection, spyware, malicious software,
+trojans and worms. Also, we accept no liability in respect of losses or damages arising out
+of changes made to the content of this website by unauthorised third parties.</p>
+<p>To provide increased value to users of this website, we may provide links to other
+websites or resources (that are not part of <a href="https://customer.benfed.in" ><strong ><u>https://customer.benfed.in</u></strong></a>) for you to access
+at your sole discretion. The user acknowledge and agree that, as the clients have chosen
+to enter the linked website we are not responsible for the availability of such external
+sites or resources, and do not review or endorse and shall not be responsible or liable,
+directly or indirectly, for (i) the privacy practices of such websites, (ii) the content of such
+websites, including (without limitation) any advertising, content, products, goods or other
+materials or services on or available from such websites or resources or (iii) the use that
+others make of these websites or resources, nor for any damage, loss or offence caused or
+alleged to be caused by, or in connection with, the use of or reliance on any such
+advertising, content, products, goods or other materials or services available on such
+external websites or resources.</p>
+<p>The above limitation or exclusion may not apply to you to the extent that applicable law
+may not allow the limitation or exclusion of liability for incidental or consequential
+damages.</p>
+<h3>Responsibility:</h3>
+<p>The BENFED takes all reasonable care to ensure that this website, namely,
+<a href="https://customer.benfed.in" ><strong ><u>https://customer.benfed.in</u></strong></a> is kept up to date, we make no warranty or representation
+that such material is accurate or reliable, or that this website will always be available, or
+that any problems will be corrected, or that it is free of viruses and the like. The BENFED
+may change the content of this website at any time without notice and without any
+liability. The BENFED makes no representation or warranty in relation to any third-party
+products and services that you may buy through our customers. So far as permitted by
+law, we will not be liable for any direct, indirect, incidental or consequential loss or
+damage that may arise out of your access, use or inability to use this website or payment
+pages of the portal.</p>
+<h3>Links:</h3>
+<p>In addition to our information on this website, there may be links to third party websites,
+the content of which is beyond the control of us. The BENFED provides such links to you as
+a convenience only. The inclusion of such links is not an endorsement by us in favour of
+any company offering any products or services on any linked website. It is the client’s
+responsibility to exercise its own skill and care with respect to any material on those
+external websites. The BENFED will not be liable for any loss or damage that may arise
+from or relating to a linked third party website.</p>
+<h3>Privacy:</h3>
+<p>Our overriding objective is to handle all data fairly and securely. Any information you give
+us about the client’s own self will be stored on our systems and may be disclosed to,
+processed and used by us, and the other companies that assist us in providing our services.</p>
+<h3>Security:</h3>
+<p>The clients must keep secret any personal identification numbers or other security codes
+that we may give to the client for access to our service, and comply with all reasonable
+instructions that the BENFED may issue in relation to keeping such personal details safe.
+The client should also make sure that any Internet payment transaction the client make is
+done in a secure environment.</p>
+<h3>Rights:</h3>
+<p>The BENFED holds the intellectual property rights in the contents of this website and all
+rights are reserved. The Client may download, store in cache, display and print the
+material and information in an unaltered form for your own personal or internal business
+purposes only provided that this notice appears with all copies. The client must not
+systematically retrieve data or other content from this website to create or compile,
+directly or indirectly, a collection, compilation, database or directory, or, copy, re-
+transmit, distribute or commercialise any material or information on this website, without
+our prior written permission.</p>
 
 
 </div>
