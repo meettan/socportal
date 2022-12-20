89b1c20af42d5b471b296fb51c098ef967cdf34e
meettan <meettan@gmail.com>
2022-12-20 18:26:40 +0530

    chnage

--- app/Http/Controllers/PayuMoneyController.php
+++ app/Http/Controllers/PayuMoneyController.php
@@ -131,58 +131,65 @@ class PayuMoneyController extends Controller
             $retHashSeq = $salt.'|'.$status.'||||||'.$udf5.'|'.$udf4.'|'.$udf3.'|'.$udf2.'|'.$udf1.'|'.$email.'|'.$firstname.'|'.$productinfo.'|'.$amount.'|'.$txnid.'|'.$key;
         }
         $hash = hash("sha512", $retHashSeq);
-        if ($hash != $posted_hash) {
-            // return "Invalid Transaction. Please try again";
-            return view('payment.validationerror');
-        } else {
+        if ($status == 'success'  && $hash == $posted_hash) {
             // echo "<h3>Thank You. Your order status is ".$status.".</h3>";
             // echo "<h4>Your Transaction ID for this transaction is ".$txnid.".</h4>";
             // echo "<h4>We have received a payment of Rs. ".$amount.". Your order will soon be shipped.</h4>";
             $udf1_explode = explode("|", $udf1);
+            if($this->verifyPayment($key,$salt,$txnid,$status)){
 
-            if (Auth::attempt(['pan' => $udf1_explode[0],'password' => $udf1_explode[1]])) {
-                $userdtl =DB::table('v_ferti_soc')
-                    ->leftJoin('v_district','v_ferti_soc.district','=','v_district.district_code')
-                    ->select('v_ferti_soc.*','v_district.district_name')
-                    ->where('v_ferti_soc.pan','=',$udf1_explode[0])
-                    ->first();
-                session(['socuserdtls' => $userdtl]);
-                Session::put('raw_password', $udf1_explode[1]);
-
-                $data = PaymentModel::where('order_id',$txnid)->first();
-                if ($data->order_id==$txnid && $data->amount==$net_amount_debit) {
-                    $data->payment_id = $mihpayid;
-                    $data->signature = $posted_hash;
-                    $data->status = $status;
-                    $data->note = $productinfo;
-                    $data->payment_at = $addedon;
-                    $data->save();
+			    $msg = "Transaction Successful, Hash Verified...Payment Verified...";
+                if (Auth::attempt(['pan' => $udf1_explode[0],'password' => $udf1_explode[1]])) {
+                    $userdtl =DB::table('v_ferti_soc')
+                        ->leftJoin('v_district','v_ferti_soc.district','=','v_district.district_code')
+                        ->select('v_ferti_soc.*','v_district.district_name')
+                        ->where('v_ferti_soc.pan','=',$udf1_explode[0])
+                        ->first();
+                    session(['socuserdtls' => $userdtl]);
+                    Session::put('raw_password', $udf1_explode[1]);
+    
+                    $data = PaymentModel::where('order_id',$txnid)->first();
+                    if ($data->order_id==$txnid && $data->amount==$net_amount_debit) {
+                        $data->payment_id = $mihpayid;
+                        $data->signature = $posted_hash;
+                        $data->status = $status;
+                        $data->note = $msg;
+                        $data->payment_at = $addedon;
+                        $data->save();
+                    }
                 }
-                // $data = new PaymentModel;
-                // $data->trans_date = date('Y-m-d');
-                // $data->payment_type = $udf4;
-                // $data->payment_mode = 'I';
-                // $data->soc_id =  $udf2;
-                // $data->brn_id = $udf3;
-                // $data->order_id = $mihpayid;
-               
-                // $data->amount = $amount;
-                // $data->invoice_id = $udf5;
-                // $data->method = $data['method'];
-                // $data->description = $details['description'];
-                // $data->card_id = $details['card_id'];
-                // $data->card = $data['card'];
-                // $data->bank = $details['bank'];
-                // $data->wallet = $details['wallet'];
-                // $data->vpa = $data['vpa'];
-                // $data->email = $email;
-                // $data->contact = $phone;
-                // $data->fee = $details['fee'];
-                // $data->tax = $details['tax'];
-               
+                return view('payment.success',['data'=>$data]);
+
+            }else{
+                $msg = "Transaction Successful, Hash Verified...Payment Verification failed...";
+                if (Auth::attempt(['pan' => $udf1_explode[0],'password' => $udf1_explode[1]])) {
+                    $userdtl =DB::table('v_ferti_soc')
+                        ->leftJoin('v_district','v_ferti_soc.district','=','v_district.district_code')
+                        ->select('v_ferti_soc.*','v_district.district_name')
+                        ->where('v_ferti_soc.pan','=',$udf1_explode[0])
+                        ->first();
+                    session(['socuserdtls' => $userdtl]);
+                    Session::put('raw_password', $udf1_explode[1]);
+    
+                    $data = PaymentModel::where('order_id',$txnid)->first();
+                    if ($data->order_id==$txnid && $data->amount==$net_amount_debit) {
+                        $data->payment_id = $mihpayid;
+                        $data->signature = $posted_hash;
+                        $data->status = "failed";
+                        $data->note = $msg;
+                        $data->payment_at = $addedon;
+                        $data->save();
+                    }
+                }
+                return view('payment.error',['data'=>$data]);
+
             }
+
+            
             // return $data;
-            return view('payment.success',['data'=>$data]);
+        }else{
+            // return "Invalid Transaction. Please try again";
+            return view('payment.validationerror');
         }
 
 
@@ -238,5 +245,110 @@ class PayuMoneyController extends Controller
         }
         
         return true;
+    }
+    public function verifyPayment($key,$salt,$txnid,$status)
+    {
+        $command = "verify_payment"; //mandatory parameter
+        
+        $hash_str = $key  . '|' . $command . '|' . $txnid . '|' . $salt ;
+        $hash = strtolower(hash('sha512', $hash_str)); //generate hash for verify payment request
+
+        $r = array('key' => $key , 'hash' =>$hash , 'var1' => $txnid, 'command' => $command);
+            
+        $qs= http_build_query($r);
+        //for production
+        //$wsUrl = "https://info.payu.in/merchant/postservice.php?form=2";
+    
+        //for test
+        $wsUrl = "https://test.payu.in/merchant/postservice.php?form=2";
+        
+        try 
+        {		
+            $c = curl_init();
+            curl_setopt($c, CURLOPT_URL, $wsUrl);
+            curl_setopt($c, CURLOPT_POST, 1);
+            curl_setopt($c, CURLOPT_POSTFIELDS, $qs);
+            curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 30);
+            curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($c, CURLOPT_SSLVERSION, 6); //TLS 1.2 mandatory
+            curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
+            $o = curl_exec($c);
+            if (curl_errno($c)) {
+                $sad = curl_error($c);
+                throw new Exception($sad);
+            }
+            curl_close($c);
+            
+            /*
+            Here is json response example -
+            
+            {"status":1,
+            "msg":"1 out of 1 Transactions Fetched Successfully",
+            "transaction_details":</strong>
+            {	
+                "Txn72738624":
+                {
+                    "mihpayid":"403993715519726325",
+                    "request_id":"",
+                    "bank_ref_num":"670272",
+                    "amt":"6.17",
+                    "transaction_amount":"6.00",
+                    "txnid":"Txn72738624",
+                    "additional_charges":"0.17",
+                    "productinfo":"P01 P02",
+                    "firstname":"Viatechs",
+                    "bankcode":"CC",
+                    "udf1":null,
+                    "udf3":null,
+                    "udf4":null,
+                    "udf5":"PayUBiz_PHP7_Kit",
+                    "field2":"179782",
+                    "field9":" Verification of Secure Hash Failed: E700 -- Approved -- Transaction Successful -- Unable to be determined--E000",
+                    "error_code":"E000",
+                    "addedon":"2019-08-09 14:07:25",
+                    "payment_source":"payu",
+                    "card_type":"MAST",
+                    "error_Message":"NO ERROR",
+                    "net_amount_debit":6.17,
+                    "disc":"0.00",
+                    "mode":"CC",
+                    "PG_TYPE":"AXISPG",
+                    "card_no":"512345XXXXXX2346",
+                    "name_on_card":"Test Owenr",
+                    "udf2":null,
+                    "status":"success",
+                    "unmappedstatus":"captured",
+                    "Merchant_UTR":null,
+                    "Settled_At":"0000-00-00 00:00:00"
+                }
+            }
+            }
+            
+            Decode the Json response and retrieve "transaction_details" 
+            Then retrieve {txnid} part. This is dynamic as per txnid sent in var1.
+            Then check for mihpayid and status.
+            
+            */
+            $response = json_decode($o,true);
+            
+            if(isset($response['status']))
+            {
+                // response is in Json format. Use the transaction_detailspart for status
+                $response = $response['transaction_details'];
+                $response = $response[$txnid];
+                
+                if($response['status'] == $status) //payment response status and verify status matched
+                    return true;
+                else
+                    return false;
+            }
+            else {
+                return false;
+            }
+        }
+        catch (Exception $e){
+            return false;	
+        }
     }
 }
