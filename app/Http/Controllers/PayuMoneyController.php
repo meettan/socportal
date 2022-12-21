67ced3d9ed5c4d992272ece0d5a8dd6143cbe9fd
meettan <meettan@gmail.com>
2022-12-21 12:34:10 +0530

    chnage

--- app/Http/Controllers/PayuMoneyController.php
+++ app/Http/Controllers/PayuMoneyController.php
@@ -130,14 +130,18 @@ class PayuMoneyController extends Controller
         } else {
             $retHashSeq = $salt.'|'.$status.'||||||'.$udf5.'|'.$udf4.'|'.$udf3.'|'.$udf2.'|'.$udf1.'|'.$email.'|'.$firstname.'|'.$productinfo.'|'.$amount.'|'.$txnid.'|'.$key;
         }
+
         $hash = hash("sha512", $retHashSeq);
         if ($status == 'success'  && $hash == $posted_hash) {
+            echo $retHashSeq;
             // echo "<h3>Thank You. Your order status is ".$status.".</h3>";
             // echo "<h4>Your Transaction ID for this transaction is ".$txnid.".</h4>";
             // echo "<h4>We have received a payment of Rs. ".$amount.". Your order will soon be shipped.</h4>";
             $udf1_explode = explode("|", $udf1);
             if($this->verifyPayment($key,$salt,$txnid,$status)){
 
+                echo "<br>";
+                echo $this->requestsendhash($key,$salt,$txnid);
 			    $msg = "Transaction Successful, Hash Verified...Payment Verified...";
                 if (Auth::attempt(['pan' => $udf1_explode[0],'password' => $udf1_explode[1]])) {
                     $userdtl =DB::table('v_ferti_soc')
@@ -254,7 +258,7 @@ class PayuMoneyController extends Controller
         $hash = strtolower(hash('sha512', $hash_str)); //generate hash for verify payment request
 
         $r = array('key' => $key , 'hash' =>$hash , 'var1' => $txnid, 'command' => $command);
-            
+        
         $qs= http_build_query($r);
         //for production
         //$wsUrl = "https://info.payu.in/merchant/postservice.php?form=2";
@@ -338,10 +342,13 @@ class PayuMoneyController extends Controller
                 $response = $response['transaction_details'];
                 $response = $response[$txnid];
                 
-                if($response['status'] == $status) //payment response status and verify status matched
+                if($response['status'] == $status) {//payment response status and verify status matched
+                    print_r($response);
                     return true;
-                else
+                }else{
+
                     return false;
+                }
             }
             else {
                 return false;
@@ -350,5 +357,44 @@ class PayuMoneyController extends Controller
         catch (Exception $e){
             return false;	
         }
+    }
+
+    public function requestsendhash($key,$salt,$txnid)
+    {
+        // $key = "7rnFly";//"qyt13u";
+        // $salt = "pjVQAWpA";
+        $command = "verify_payment";
+        $var1 = $txnid; // Transaction ID
+
+
+        $hash_str = $key  . '|' . $command . '|' . $var1 . '|' . $salt ;
+        $hash = strtolower(hash('sha512', $hash_str));
+
+            $r = array('key' => $key , 'hash' =>$hash , 'var1' => $var1, 'command' => $command);
+            return $hash;
+            // die();
+            $qs= http_build_query($r);
+            $wsUrl = "https://test.payu.in/merchant/postservice.php?form=1";
+            //$wsUrl = "https://info.payu.in/merchant/postservice?form=1";
+            $c = curl_init();
+            curl_setopt($c, CURLOPT_URL, $wsUrl);
+            curl_setopt($c, CURLOPT_POST, 1);
+            curl_setopt($c, CURLOPT_POSTFIELDS, $qs);
+            curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 30);
+            curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
+            $o = curl_exec($c);
+            if (curl_errno($c)) {
+            $sad = curl_error($c);
+            throw new Exception($sad);
+            }
+            curl_close($c);
+
+            $valueSerialized = @unserialize($o);
+            if($o === 'b:0;' || $valueSerialized !== false) {
+            print_r($valueSerialized);
+            }
+            print_r($o);
     }
 }
